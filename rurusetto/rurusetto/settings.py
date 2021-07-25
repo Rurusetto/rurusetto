dfeb8b0f75fdabe79f6fd10e79ca8c4c71eb5cdc
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-26 01:16:41 +0700

    Start making apps

--- rurusetto/rurusetto/settings.py
+++ rurusetto/rurusetto/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ rurusetto/wiki/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WikiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wiki'

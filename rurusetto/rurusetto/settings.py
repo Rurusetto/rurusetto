c8f3be15011749d80460cc1f34f256adb229de69
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-07-26 02:02:27 +0700

    Add necessary file to start a site

--- rurusetto/rurusetto/settings.py
+++ rurusetto/rurusetto/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'wiki.apps.WikiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

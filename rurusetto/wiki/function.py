0f0f60d6115b21922ccc23e61059cb3b04b28c9b
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-03 05:21:55 +0700

    Change function use

--- rurusetto/wiki/function.py
+++ rurusetto/wiki/function.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 def make_listing_view(model_list):
     export_list = []
     for item in model_list:
-        ruleset_owner = User.objects.filter(id=item.owner).first()
+        ruleset_owner = User.objects.get(id=item.owner)
         export_list.append([item, ruleset_owner])
     return export_list

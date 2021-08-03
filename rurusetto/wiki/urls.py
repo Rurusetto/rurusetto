a0315733fe34813d262c3ce0446e54f85fd4071e
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-04 01:10:35 +0700

    Change listing url from `listing` to `rulesets`

--- rurusetto/wiki/urls.py
+++ rurusetto/wiki/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('listing/', views.listing, name='listing'),
+    path('rulesets/', views.listing, name='listing'),
     path('new/', views.create_ruleset, name='create_ruleset'),
     path('changelog/', views.changelog, name='changelog'),
     path('rulesets/<slug:slug>', views.wiki_page, name='wiki'),

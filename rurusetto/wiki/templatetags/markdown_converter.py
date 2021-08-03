3fe86568ca2d14b4cad6e1f3d073e787781c3a22
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-04 01:10:05 +0700

    Add markdown converter ability to highlight a code in wiki
    Will add a config to make user can change highlight color.

--- rurusetto/wiki/templatetags/markdown_converter.py
+++ rurusetto/wiki/templatetags/markdown_converter.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 def convert_markdown(value):
-    return markdown.markdown(value)
+    return markdown.markdown(value, extensions=['fenced_code', 'codehilite', 'tables'])
 
 
 register.filter('convert_markdown', convert_markdown)

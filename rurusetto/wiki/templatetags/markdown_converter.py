2b1a0d29dca6f30de8dbfd87032aaccb9e8c1360
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-06 00:43:58 +0700

    Fix the converter is not break line when the markdown file is break the line

--- rurusetto/wiki/templatetags/markdown_converter.py
+++ rurusetto/wiki/templatetags/markdown_converter.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 def convert_markdown(value):
-    return markdown.markdown(value, extensions=['fenced_code', 'codehilite', 'tables'])
+    return markdown.markdown(value, extensions=['fenced_code', 'codehilite', 'tables', 'nl2br'])
 
 
 register.filter('convert_markdown', convert_markdown)

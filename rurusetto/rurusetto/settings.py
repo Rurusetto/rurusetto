b2d742d8edddc8734933c1427832caa1a0e49965
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-10 18:07:19 +0700

    Update site settings

--- rurusetto/rurusetto/settings.py
+++ rurusetto/rurusetto/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

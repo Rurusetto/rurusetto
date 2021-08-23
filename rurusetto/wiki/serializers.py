2a56fd16a6dfc8042a88a14416bc3aa9f0df37bc
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-24 01:39:59 +0700

    Add wiki content to wiki API

--- rurusetto/wiki/serializers.py
+++ rurusetto/wiki/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from .models import Ruleset
 
 
-class RulesetSerializer(serializers.ModelSerializer):
+class RulesetSerializer(serializers.HyperlinkedModelSerializer):
     """
     Serializer for Ruleset model for passing the value to the API view
     """
     class Meta:
         model = Ruleset
-        fields = ['id', 'name', 'slug', 'description', 'icon', 'logo', 'cover_image', 'opengraph_image', 'source',
+        fields = ['id', 'name', 'slug', 'description', 'icon', 'logo', 'cover_image', 'opengraph_image', 'source', 'content',
                   'creator', 'owner', 'last_edited_by', 'created_at', 'last_edited_at', 'verified']

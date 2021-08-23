1d24b778d03af902747d7e4e98c21c228c77a74a
Phawit Pornwattanakul <68165621+HelloYeew@users.noreply.github.com>
2021-08-23 19:30:42 +0700

    Add more field on ruleset API

--- rurusetto/wiki/serializers.py
+++ rurusetto/wiki/serializers.py
@@ -3,6 +3,10 @@ from .models import Ruleset
 
 
 class RulesetSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Ruleset model for passing the value to the API view
+    """
     class Meta:
         model = Ruleset
-        fields = ['id', 'name', 'slug', 'description', 'icon', 'logo', 'source']
+        fields = ['id', 'name', 'slug', 'description', 'icon', 'logo', 'cover_image', 'opengraph_image', 'source',
+                  'creator', 'owner', 'last_edited_by', 'created_at', 'last_edited_at', 'verified']

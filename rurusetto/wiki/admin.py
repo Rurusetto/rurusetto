a6edeb2510904f915d6adedd1c57740423d34162
Phawit Pornwattanakul <me@helloyeew.dev>
2021-08-26 02:34:24 +0700

    Add subpage to the admin page

--- rurusetto/wiki/admin.py
+++ rurusetto/wiki/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Changelog, Ruleset, CustomWiki
+from .models import Changelog, Ruleset, CustomWiki, Subpage
 
 # Register your models here.
 
 admin.site.register(Changelog)
 admin.site.register(Ruleset)
 admin.site.register(CustomWiki)
+admin.site.register(Subpage)

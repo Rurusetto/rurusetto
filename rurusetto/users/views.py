e6cb4fc273cd79d673d34b04e576b88d9c4313e8
Phawit Pornwattanakul <me@helloyeew.dev>
2021-08-30 21:45:20 +0700

    Remove unused import

--- rurusetto/users/views.py
+++ rurusetto/users/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import check_password
 from django.shortcuts import render, redirect, get_object_or_404, resolve_url
 from django.contrib import messages
 from django.templatetags.static import static

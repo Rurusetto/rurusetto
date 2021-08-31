0fbc23c0a960126d05fc10b0832e47a955901c31
Phawit Pornwattanakul <me@helloyeew.dev>
2021-09-01 00:56:31 +0700

    Delete some unused import in user form

--- rurusetto/users/forms.py
+++ rurusetto/users/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import Profile, Config, THEME, SUBPAGE_INDEX
 
 

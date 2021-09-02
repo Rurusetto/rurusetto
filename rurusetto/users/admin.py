4303e85c000af178ca605277b710f07c8142b2a2
Phawit Pornwattanakul <me@helloyeew.dev>
2021-09-02 19:17:07 +0700

    Add tag to admin page

--- rurusetto/users/admin.py
+++ rurusetto/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Config
+from .models import Profile, Config, Tag
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Config)
+admin.site.register(Tag)

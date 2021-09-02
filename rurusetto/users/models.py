1484cbcd4b91808ad918c1c87b6e69bf5119c4c9
Phawit Pornwattanakul <me@helloyeew.dev>
2021-09-03 00:49:39 +0700

    Change max string length in tag model

--- rurusetto/users/models.py
+++ rurusetto/users/models.py
@@ -92,7 +92,7 @@ class Tag(models.Model):
     name = models.CharField(default="Default tag", max_length=25)
     pills_color = ColorField(default="#FF66AA")
     font_color = ColorField(default="#FFFFFF")
-    description = models.CharField(default="", max_length=500)
+    description = models.CharField(default="", max_length=200)
 
     def __str__(self):
         return self.name

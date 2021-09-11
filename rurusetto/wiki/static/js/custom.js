9b299aea8b7289251ab76eeaa39f20f98502b9db
Phawit Pornwattanakul <me@helloyeew.dev>
2021-09-12 04:56:16 +0700

    Move some JS that is use only one page to that page's template

--- rurusetto/wiki/static/js/custom.js
+++ rurusetto/wiki/static/js/custom.js
@@ -160,7 +160,4 @@ AOS.init();
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-$(document).ready(function () {
-    $('.mediPlayer').mediaPlayer();
 });

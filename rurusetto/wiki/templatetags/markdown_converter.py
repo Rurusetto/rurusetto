b81f6f6cc003465c5637f94e1c3697ab9f50876d
Phawit Pornwattanakul <me@helloyeew.dev>
2021-09-25 02:14:17 +0700

    Add new extension for support markdown conversion

--- rurusetto/wiki/templatetags/markdown_converter.py
+++ rurusetto/wiki/templatetags/markdown_converter.py
@@ -5,7 +5,8 @@ register = template.Library()
 
 
 def convert_markdown(value):
-    return markdown.markdown(value, extensions=['fenced_code', 'codehilite', 'tables', 'nl2br'])
+    return markdown.markdown(value, extensions=['fenced_code', 'codehilite', 'tables', 'nl2br', 'toc', 'wikilinks', 
+                                                'attr_list'])
 
 
 register.filter('convert_markdown', convert_markdown)

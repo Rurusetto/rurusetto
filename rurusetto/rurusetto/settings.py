80663056bb9ef95a40586473a0153f7213aa0921
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-08 02:35:43 +0700

    Add test server setting to server

--- rurusetto/rurusetto/settings.py
+++ rurusetto/rurusetto/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default="an_awesome_secret_key_here")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
+TEST_SERVER = config('TEST_SERVER', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='')
 

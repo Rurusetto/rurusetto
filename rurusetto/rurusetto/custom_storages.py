e1b86cdc0b137ff01dbedb9011c68ddc35ac23d1
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-10 03:19:41 +0700

    Change dummy bucket name

--- rurusetto/rurusetto/custom_storages.py
+++ rurusetto/rurusetto/custom_storages.py
@@ -1,11 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from decouple import config
 
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = '<your_space_name>'
+    bucket_name = config('AWS_STORAGE_BUCKET_NAME', default="your-storage-bucket-name")
     location = 'static'
 
 
 class MediaStorage(S3Boto3Storage):
-    bucket_name = '<your_space_name>'
+    bucket_name = config('AWS_STORAGE_BUCKET_NAME', default="your-storage-bucket-name")
     location = 'media'

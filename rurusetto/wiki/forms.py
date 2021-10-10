5f700d3271289a4b0696b1e55b665775f164336c
Phawit Pornwattanakul <me@helloyeew.dev>
2021-10-10 21:49:48 +0700

    Add hidden to Ruleset field

--- rurusetto/wiki/forms.py
+++ rurusetto/wiki/forms.py
@@ -12,11 +12,12 @@ class RulesetForm(forms.ModelForm):
     description = forms.CharField(required=True, widget=forms.Textarea)
     content = MDTextFormField()
     source = forms.URLField(label="Source")
+    hidden = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': "form-check-input"}))
 
     class Meta:
         model = Ruleset
         fields = ['name', 'description', 'icon', 'light_icon', 'logo', 'cover_image', 'cover_image_light',
-                  'opengraph_image', 'recommend_beatmap_cover', 'custom_css', 'content', 'source', 'github_download_filename']
+                  'opengraph_image', 'recommend_beatmap_cover', 'custom_css', 'content', 'source', 'github_download_filename', 'hidden']
 
 
 class SubpageForm(forms.ModelForm):

301642555fb8803d3afb2d74bd24ed40a5d002a6
Phawit Pornwattanakul <me@helloyeew.dev>
2021-12-27 02:15:56 +0700

    Rewrite the serializer for getting better result in response

--- rurusetto/api/serializers.py
+++ rurusetto/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from wiki.models import Ruleset
+from users.models import Profile
+from django.contrib.auth.models import User
 
 
 class RulesetSerializer(serializers.HyperlinkedModelSerializer):
@@ -21,3 +23,39 @@ class MinimizeRulesetSerializer(serializers.HyperlinkedModelSerializer):
         model = Ruleset
         fields = ['id', 'name', 'slug', 'description', 'icon', 'light_icon', 'source', 'github_download_filename',
                   'owner', 'verified', 'archive']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+# Serializer for listing page
+
+
+class ProfileListingSerializer(serializers.ModelSerializer):
+    """
+    Serializer that will only filter the profile of owner to use in RulesetListingSerializer.
+    """
+    user = UserSerializer()
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'image']
+        depth = 1
+
+
+class RulesetListingSerializer(serializers.ModelSerializer):
+    """
+    Serializer for essential information that is user in listing page.
+    """
+    owner_detail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ruleset
+        fields = ['id', 'name', 'slug', 'description', 'icon', 'light_icon', 'owner_detail']
+
+    def get_owner_detail(self, obj):
+        owner = Profile.objects.get(id=obj.owner)
+        return ProfileListingSerializer(owner).data

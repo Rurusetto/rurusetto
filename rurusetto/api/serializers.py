b1e7592de0b34b15f25f2dc53eaa29e63751fbf1
Phawit Pornwattanakul <me@helloyeew.dev>
2021-12-28 01:20:47 +0700

    Remove unused serializer

--- rurusetto/api/serializers.py
+++ rurusetto/api/serializers.py
@@ -4,27 +4,6 @@ from users.models import Profile
 from django.contrib.auth.models import User
 
 
-class FullRulesetSerializer(serializers.HyperlinkedModelSerializer):
-    """
-    Serializer for Ruleset model for passing the value to the API view
-    """
-    class Meta:
-        model = Ruleset
-        fields = ['id', 'name', 'slug', 'description', 'icon', 'light_icon', 'logo', 'cover_image', 'cover_image_light',
-                  'opengraph_image', 'recommend_beatmap_cover', 'custom_css', 'content', 'source', 'github_download_filename',
-                  'creator', 'created_at', 'owner', 'last_edited_at', 'last_edited_by', 'verified', 'archive']
-
-
-class MinimizeRulesetSerializer(serializers.HyperlinkedModelSerializer):
-    """
-    Serializer for Ruleset model for passing only main detail of the ruleset to the API view
-    """
-    class Meta:
-        model = Ruleset
-        fields = ['id', 'name', 'slug', 'description', 'icon', 'light_icon', 'source', 'github_download_filename',
-                  'owner', 'verified', 'archive']
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User

d4d22a30bf6a0bc821d8a450a9728871921b9990
Phawit Pornwattanakul <me@helloyeew.dev>
2023-02-07 09:33:27 +0700

    Fix some tuple in database enum cannot call

--- rurusetto/users/models.py
+++ rurusetto/users/models.py
@@ -26,7 +26,7 @@ LANGUAGE = (
     # Current available language
     # In-system value - Show value
     ('en', 'English (Default)'),
-    ('es', 'Spanish')
+    ('es', 'Spanish'),
     ('fr', 'French (Not complete)'),
     ('th', 'Thai')
 )
